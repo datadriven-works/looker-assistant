d233f28c57710c719a2c91c7f2d202c49cf5a4da
Jawad Laraqui <laraqui@gmail.com>
2025-03-27 13:17:40 -0400

    Update environment variables, add crypto-js dependency, and enhance ChatSurface functionality
    - Updated .env_example to include new environment variables for Vertex AI.
    - Added crypto-js as a dependency in package.json and updated yarn.lock.
    - Enhanced ChatSurface component to utilize a new generateContent hook for processing chat messages and generating content.
    - Introduced a history generation function to format chat messages for content generation.

--- src/components/ChatSurface/index.tsx
+++ src/components/ChatSurface/index.tsx
@@ -5,17 +5,64 @@ import { useCallback, useEffect, useRef } from 'react'
 import {
   addMessage,
   AssistantState,
+  ChatMessage,
   setIsQuerying,
   setQuery,
   TextMessage,
 } from '../../slices/assistantSlice'
 import { RootState } from '../../store'
 import { v4 as uuidv4 } from 'uuid'
+import { useGenerateContent } from '../../hooks/useGenerateContent'
+
+const generateHistory = (messages: ChatMessage[]) => {
+  const history: any[] = []
+  messages.forEach((oneMessage: ChatMessage) => {
+    const parts = []
+    let role = ''
+    if (oneMessage.type === 'functionCall') {
+      role = 'model'
+
+      parts.push({
+        functionCall: {
+          id: oneMessage.uuid,
+          name: oneMessage.name,
+          args: oneMessage.args || {},
+        },
+      })
+    } else if (oneMessage.type === 'text') {
+      role = oneMessage.actor
+      parts.push(oneMessage.message)
+    } else if (oneMessage.type === 'functionResponse') {
+      role = 'user'
+      parts.push({
+        functionResponse: {
+          id: oneMessage.callUuid,
+          name: oneMessage.name,
+          response: {
+            name: oneMessage.name,
+            content: oneMessage.response,
+          },
+        },
+      })
+    }
+
+    history.push({
+      role,
+      parts,
+    })
+  })
+
+  return history
+}
+
 const ChatSurface = () => {
   const dispatch = useDispatch()
   const endOfMessagesRef = useRef<HTMLDivElement>(null) // Ref for the last message
+  const { generateContent } = useGenerateContent()
 
-  const { query, isQuerying } = useSelector((state: RootState) => state.assistant as AssistantState)
+  const { query, isQuerying, thread } = useSelector(
+    (state: RootState) => state.assistant as AssistantState
+  )
 
   const scrollIntoView = useCallback(() => {
     endOfMessagesRef.current?.scrollIntoView({ behavior: 'smooth' })
@@ -49,6 +96,17 @@ const ChatSurface = () => {
       type: 'text',
     }
     dispatch(addMessage(responseMessage))
+    const contentList: ChatMessage[] = [...(thread?.messages || [])]
+    const tools: any[] = []
+    const systemInstruction = ''
+
+    const response = await generateContent({
+      contents: generateHistory(contentList),
+      tools,
+      systemInstruction,
+    })
+
+    console.log(response)
 
     dispatch(setIsQuerying(false))
     dispatch(setQuery(''))
@@ -70,7 +128,7 @@ const ChatSurface = () => {
     <>
       <div className="flex-grow overflow-y-auto max-h-full mb-36 ">
         <div className="max-w-4xl mx-auto mt-8">
-          <Thread />
+          <Thread endOfMessagesRef={endOfMessagesRef} />
         </div>
       </div>
       <div
