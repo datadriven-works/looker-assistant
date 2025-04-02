d79dc4daf44984fb327d6cc405b7bc0f79234733
Jawad Laraqui <laraqui@gmail.com>
2025-04-02 10:20:02 -0400

    Refactor FunctionCallMessage to improve function name display
    - Updated the FunctionCallMessage component to format function names by replacing underscores with spaces and converting camel case to words for better readability.
    - Enhanced the Chip label to reflect the new formatting, improving user experience in the chat interface.

--- src/components/ChatSurface/FunctionCallMessage.tsx
+++ src/components/ChatSurface/FunctionCallMessage.tsx
@@ -3,11 +3,18 @@ import Chip from '@mui/material/Chip'
 import clsx from 'clsx'
 
 const FunctionCallMessage = ({ message }: { message: FunctionCall }) => {
+  const functionName = message.name.replace(/_/g, ' ')
+  // camel case to words
+  const functionNameWords = functionName
+    .replace(/([A-Z])/g, ' $1')
+    .trim()
+    .toLowerCase()
+
   return (
     <div className={`flex justify-start mb-4`}>
       <div className={`max-w-[70%]`}>
         <div className={clsx('rounded-lg p-3 max-w-xl')}>
-          <Chip label={message.name.replace(/_/g, ' ')} />
+          <Chip label={functionNameWords} />
         </div>
       </div>
     </div>
